6d28fcf37273f58876d9e7a25623e75bc284f9b6
Fadhil Yaacob <mohamadfadhil.yaacob@onapp.com>
2021-01-13 23:25:53 +0800

    Setup todo and login page

---
+++ frontend/pages/todo.vue
@@ -0,0 +1,24 @@
+<template>
+  <section class="section">
+    <ul v-for="todo in todos" :key="todo.id">
+      <li>{{ todo.title }}</li>
+    </ul>
+  </section>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: "milk the cow",
+        },
+      ],
+    };
+  },
+  created() {
+    // request to API server for list of items
+  }
+};
+</script>
